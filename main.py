934bf7de95517eb1fca083eab781d631e8108827
AlanPL <apyolt@gmail.com>
2021-12-09 12:45:22 -0600

    Description of all functionality

---
+++ main.py
@@ -0,0 +1,31 @@
+print('dkf')
+#MENU:
+#for alice:
+#sign artwork previously requesting the name of the artist and showing the message of ownership that will be added:
+#verify final document received by notary
+#show final document
+
+#for bob:
+#verify and show document received by alice
+#sign document of not use without authorization previosly requesting the name
+#verify final document received by notary
+#show final document
+
+#for notary:
+#verify and show document received by bob and alice
+#sign document of validation if the notary decide it 
+
+
+#FUNCTIONS:
+#generate private and public passwords
+
+#sign artwork and document of ownership
+#verify artwork and document of ownership
+
+#sign document of not use without authorization
+#verify document of not use without authorization
+
+#sign document of validation of both signatures
+#verify document of validation of both signatures
+
+#encrypt both documents
