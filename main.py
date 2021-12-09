f2e18dd16758de8a9faadd5cb7cf51fb8cc5fe15
Josefina Tovar <josefina10969@gmail.com>
2021-12-09 15:09:03 -0600

    Add main menu

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-print('dkf')
-#MENU:
+def askMenuOption():
+    validNumber = False
+    number = 0
+    while(not validNumber):
+        try:
+            number = int(input("Choose an option: "))
+            validNumber = True
+        except ValueError:
+            print("Error, please enter an int number")   
+    return number
+exit = False
+option = 0
+while not exit:  
+    print("****************************************")  
+    print("Choose what you want to do")  
+    print ("1. Sign your art")
+    print ("2. Sign agreement not to use without the consent of the artist")
+    print ("3. Verify both signs")
+    print ("4. Exit")
+
+    option = askMenuOption()
+ 
+    if option == 1:
+        print ("Option 1")
+    elif option == 2:
+        print ("Option 2")
+    elif option == 3:
+        print("Option 3")
+    elif option == 4:
+        exit = True
+    else:
+        print ("Choose a number among 1 and 3")
+print ("Bye")
+#print(f"You choose {option}")
+
 #for alice:
 #sign artwork previously requesting the name of the artist and showing the message of ownership that will be added:
 #verify final document received by notary
