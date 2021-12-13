031b94df5f50917dfade10d3aac685afaaad1178
SebastianCD <43630732+SebastianCD@users.noreply.github.com>
2021-12-13 17:40:10 -0600

    Se corrigió el algoritmo de RSA

--- RSA.py
+++ RSA.py
@@ -1,34 +1,31 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
+import base64
 
 def keyGeneration():
     #Generar pareja de claves RSA de 2048 bits de longitud
     key = RSA.generate(2048)
 
-    #Encrypt the private key
-    secret_code = "12345"
-    private_key = key.export_key(passphrase=secret_code)
+    #Private key
+    private_key = key.export_key()
 
     #Save the private key in a .pem file
-    with open("private.pem", "wb") as f:
+    with open("public.pem", "wb") as f:
         f.write(private_key)
 
     #Obtain the public key
     public_key = key.publickey().export_key()
 
     #Save the public key in a .pem file
-    with open("public.pem", "wb") as f:
+    with open("private.pem", "wb") as f:
         f.write(public_key)
 
-def encryption():
-    #Example of string to encrypt
-    s = "Pongame 10"
-
+def encryption(priv ,s):
     #Convert the string into bytes
-    byteData = s.encode("utf-8")
+    byteData = s.encode("ascii")
 
     #Read the file with the public key
-    with open("public.pem", "rb") as f:
+    with open(priv, "rb") as f:
         recipient_key = f.read()
     key = RSA.importKey(recipient_key)
 
@@ -40,11 +37,11 @@ def encryption():
 
     return encData
 
-def decryption(encData):
+def decryption(pub, encData):
     #Read the file with the public key
-    with open("private.pem", "rb") as f:
+    with open(pub, "rb") as f:
         recipient_key = f.read()
-    key = RSA.importKey(recipient_key, passphrase="12345")
+    key = RSA.importKey(recipient_key)
     
     #Instance of the cipher
     cipher_rsa = PKCS1_OAEP.new(key)
@@ -53,6 +50,44 @@ def decryption(encData):
     decData = cipher_rsa.decrypt(encData)
 
     #Convert the byte string to characters
-    s = decData.decode("utf-8")
+    s = decData.decode("ascii")
 
     return s
+
+def readDoc(filetext):
+    f = open (filetext,'r')
+    s = f.read()
+    f.close()
+    return s
+
+def readDoc2(filetext):
+    f = open (filetext,'rb')
+    s = f.read()
+    f.close()
+    s = base64.b64decode(s)
+    return s
+
+def saveDoc(filetext,text):
+    f = open (filetext,'wb')
+    f.write(base64.b64encode(text))
+    f.close()
+
+def saveDoc2(filetext,text):
+    f = open (filetext,'w')
+    f.write(text)
+    f.close()
+
+file_text=input("Nombre del archivo: ")
+file_text2=input("Nombre del archivo para texto cifrado: ")
+file_text3=input("Nombre del archivo para texto descifrado: ")
+
+priv = "private.pem"
+pub = "public.pem"
+
+s = readDoc(file_text)
+keyGeneration()
+cip_text = encryption(priv, s)
+saveDoc(file_text2,cip_text)
+s2 = readDoc2(file_text2)
+dec_text = decryption(pub,s2)
+saveDoc2(file_text3,dec_text)
